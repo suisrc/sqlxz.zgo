1fe2ac8d191ee8c1a556637e345eaf6b4747cada
yingshuai.lin <yingshuai.lin@mecoolchina.cn>
2021-04-16 11:22:00 +0800

    fix update

--- update.go
+++ update.go
@@ -1,15 +1,23 @@
 package sqlxz
 
+import "errors"
+
 // UpdateAndSaveX ...
-func UpdateAndSaveX(sqlx ExecDB, srs interface{}, tbl2 string, id int64, fid func(int64)) error {
-	sic := TableIdxColumn{Table: TBL + tbl2, IDVal: id}
+func UpdateAndSaveX(sqlx ExecDB, srs interface{}, tbl string, sid int64, fid func(int64)) error {
+	sic := TableIdxColumn{Table: TBL + tbl, IDVal: sid}
 	if sqr, sps, err := CreateUpdateSQLByNamedAndSkipNilAndSet(sic, srs); err != nil {
 		return err
 	} else if res, err := sqlx.NamedExec(sqr, sps); err != nil {
 		return err
-	} else if id == 0 && fid != nil {
-		if lii, err := res.LastInsertId(); err == nil {
-			fid(lii)
+	} else if cii, err := res.RowsAffected(); err != nil {
+		return err
+	} else if cii == 0 { // 操作失败
+		return errors.New("sql: no rows in result set")
+	} else if sid == 0 { // install id
+		if nid, err := res.LastInsertId(); err != nil {
+			return err
+		} else {
+			fid(nid)
 		}
 	}
 	return nil
@@ -21,6 +29,10 @@ func UpdateAndSaveTIC(sqlx ExecDB, srs interface{}, sic *TableIdxColumn, fres fu
 		return err
 	} else if res, err := sqlx.NamedExec(sqr, sps); err != nil {
 		return err
+	} else if cii, err := res.RowsAffected(); err != nil {
+		return err
+	} else if cii == 0 { // 操作失败
+		return errors.New("sql: no rows in result set")
 	} else if fres != nil {
 		fres(res)
 	}
